e5aa71191ec8906ac5e2d67f67405cf637ff6dac
Ghanna <stavka1000@gmail.com>
2019-09-21 13:30:56 +0300

    all types Java

---
+++ src/main/java/sMalenkoyBukvi/Main.java
@@ -0,0 +1,15 @@
+package sMalenkoyBukvi;
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("Aloha world");
+        int number = 0; //4b
+        float floatNumber = 5.6f; //8b
+        long longNumber = 34535341; //8b
+        boolean booleanFlag = false; //
+        byte byteNumber = 4;
+        double dobleNumber = 5.5;
+        //char
+        //short
+    }
+}
